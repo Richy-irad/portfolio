eb1b3b911d1fa26476354d3b7fa24c61e4a870ae
Richy-irad <richardirad@gmail.com>
2024-04-13 15:55:28 -0400

    add Popover menu on mobile views

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Joti_One, Montserrat } from "next/font/google";
+
+export const jotiOne = Joti_One({ weight: "400", subsets: ["latin"] });
+export const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import { Joti_One, Montserrat } from "next/font/google";
-
-const jotiOne = Joti_One({ weight: "400", subsets: ["latin"] });
-const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+import { jotiOne, montserrat } from "@/app/fonts";
+import { Bars2Icon } from "@heroicons/react/24/outline";
+import { X } from "@phosphor-icons/react";
+import { Popover } from "@headlessui/react";
 
 const links = [
   { name: "About", href: "/#about", number: "01." },
@@ -17,7 +17,7 @@ const links = [
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex items-center justify-between px-5 md:px-10 lg:px-20 py-7 snap-start">
+    <nav className="fixed top-0 w-full flex items-center justify-between px-5 md:px-10 lg:px-20 py-7 snap-start bg-background-100">
       <Link
         href="/"
         className={`${jotiOne.className} text-foreground text-4xl uppercase`}
@@ -45,6 +45,41 @@ export default function Navbar() {
       >
         resume
       </Link>
+      <Popover className="flex lg:hidden">
+        <Popover.Button>
+          <Bars2Icon className="w-8 h-8 text-foreground" />
+        </Popover.Button>
+        <Popover.Overlay className="fixed inset-0" />
+        <Popover.Panel className="fixed left-0 top-0 w-full md:w-96 h-screen z-10 flex flex-col gap-6 bg-dark-100 p-10">
+          {({ close }) => (
+            <>
+              {/* close button */}
+              <button
+                type="button"
+                className="text-foreground self-end"
+                onClick={() => close()}
+              >
+                <X size={24} weight="bold" />
+              </button>
+
+              {/* the nav items */}
+              {links.map((link, index) => (
+                <Popover.Button
+                  key={index}
+                  as={Link}
+                  href={link.href}
+                  className="flex gap-1.5 items-center text-xl"
+                >
+                  <span className={`${montserrat.className} text-foreground`}>
+                    {link.number}
+                  </span>
+                  <span className="text-light-100">{link.name}</span>
+                </Popover.Button>
+              ))}
+            </>
+          )}
+        </Popover.Panel>
+      </Popover>
     </nav>
   );
 }

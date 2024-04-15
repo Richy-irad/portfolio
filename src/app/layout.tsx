ab702c6134675d30086a0af6961e2018df1213c9
Richy-irad <richardirad@gmail.com>
2024-04-15 16:58:45 -0400

    fix styling issue in projects page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth snap-y">
       <body className={`bg-background-100 text-light-300 ${inter.className}`}>
         <Navbar />
-        <main className="mx-5 md:mx-10 lg:mx-20 xl:mx-40 2xl:mx-96 h-fit pb-24 mt-20">
+        <main className="mx-5 md:mx-10 lg:mx-20 xl:mx-40 2xl:mx-96 h-fit pb-24 mt-20 lg:mt-24">
           {children}
           <Analytics />
           <SpeedInsights />
